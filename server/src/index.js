d43bd16ba1074b2b5e05737d7e7aa720452c2eb3
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-09-12 00:53:52 +0500

    update

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,25 @@
 const { ApolloServer, gql } = require("apollo-server");
 
-const users = require("./data");
+const users = require("./data/users");
+const messages = require("./data/messages");
+
+// function indexNumber(source) {
+//   return Math.round(Math.random() * (source.length - 1));
+// }
 
 const typeDefs = gql`
   type Query {
     users: [User!]
     user(id: ID!): User
+    me: User
+    message(id: ID!): Message
+    messages: [Message!]!
   }
 
-  type Icas {
-    user(id: ID!): String
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
   }
 
   type User {
@@ -29,26 +39,30 @@ const resolvers = {
   Query: {
     users: () => users,
     user: (parent, { id }) => users.find(({ id: userId }) => userId === Number(id)),
+    me: (parent, args, { me }) => me,
+    message: (parent, { id }) => messages.find((message) => message.id === Number(id)),
+    messages: () => messages,
   },
+
   Mutation: {
     user: (parent, args) => {
-      users.push({
-        id: args.id || 564,
-        firstName: args.firstName,
-        lastName: args.lastName,
-        email: args.email,
-        password: args.password,
-      });
-      const usera = users.slice(-1);
-      console.log(usera[0]);
-      return usera[0];
+      users.push({ id: users.length + 1, firstName: args.firstName, lastName: args.lastName, email: args.email, password: args.password });
+      return users.slice(-1)[0];
     },
   },
+  Message: {
+    user: (message) => users.find((user) => user.id === Number(message.userId)),
+  },
 };
+
+const indexNumber = (source) => Math.round(Math.random() * (source.length - 1));
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: {
+    me: users[indexNumber(users)],
+  },
 });
 
 server.listen({ port: 8080 }).then(({ url }) => console.log(`GraphQL server running at ${url}`));
