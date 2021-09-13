846dee826ae120dc3c1f78cf0d0e4af417aa0007
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-09-13 22:43:01 +0500

    apollo-server-express

--- server/src/index.js
+++ server/src/index.js
@@ -17,3 +17,27 @@ const server = new ApolloServer({
 });
 
 server.listen({ port: 8080 }).then(({ url }) => console.log(`GraphQL server running at ${url}`));
+
+
+
+
+// SERVER SETUP WITH  *** apollo-server-express ***
+
+// const { ApolloServer } = require("apollo-server-express");
+// const app = require("express")();
+
+// const typeDefs = require("./schema");
+// const resolvers = require("./resolvers");
+
+// const startApolloServer = async (typeDefs, resolvers) => {
+//   const server = new ApolloServer({ typeDefs, resolvers });
+//   await server.start();
+
+//   server.applyMiddleware({ app });
+
+//   // Modified server startup
+//   await new Promise((resolve) => app.listen({ port: 8080 }, resolve));
+//   console.log(`🚀 Server ready at http://localhost:8080${server.graphqlPath}`);
+// };
+
+// startApolloServer(typeDefs, resolvers);
